b18cebd1c22ce6474509aa7e73265a2f847c5b0b
Ante-237 <akumawahanye@gmail.com>
2023-01-30 22:51:17 +0200

    working for __str__ tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -72,7 +72,7 @@ class TestCaseBaseModel(unittest.TestCase):
 
     def test_str_(self):
         """ test for string print function """
-        self.assertEqual(print(self.my_model), print(self.my_model))
+        # self.assertEqual(print(self.my_model), print(self.my_model))
 
     def test_sizeofDict(self):
         """ensuring the dictionary is the expected length
